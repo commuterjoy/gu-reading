c0f8818144a8063d7d5073148b49de06c1c7590e
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-03-15 12:02:37 +0000

    Fixed marked

--- docs.sh
+++ docs.sh
@@ -1,7 +1,7 @@
-node app/node_modules/marked/bin/marked -o README.html < README.md 
+marked -o README.html < README.md 
 cat css >> README.html
 
 # section graphs
-node app/node_modules/marked/bin/marked -o world.html < world.md
-node app/node_modules/marked/bin/marked -o uk.html < uk.md
+marked -o world.html < world.md
+marked -o uk.html < uk.md
 
